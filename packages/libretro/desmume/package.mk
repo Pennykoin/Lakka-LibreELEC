e742e6e039ba827f80bc1bf21b421d0b93eddd70
Jerry Howell <39189962+Pennykoin@users.noreply.github.com>
2018-09-17 21:35:17 -0400

    another commit update for db410

--- packages/libretro/desmume/package.mk
+++ packages/libretro/desmume/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="desmume"
-PKG_VERSION="b59b3be"
+PKG_VERSION="f5462d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
